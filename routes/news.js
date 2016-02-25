73568415a8861aa2338969c04f4c0748bff2f9ae
carpcai <563998753@qq.com>
2016-02-25 14:54:28 +0800

    bug

--- routes/news.js
+++ routes/news.js
@@ -12,9 +12,27 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+router.get('/', function(req, res, next) {
+  res.render('news', { title: 'dgut.ren', message: 'news api !'});
+});
+
 /* GET later news api. */
 router.get('/later_news', function(req, res, next) {
   userGetSql = 'Select id,title,pubdate From `dede_archives` where channel=1 And arcrank = 0 order by id desc limit 0,10';
+  connection.query(userGetSql,function (err, result) {
+  if(err){
+    console.log('[SELECT ERROR] - ',err.message);
+    res.status(500).send("error")
+    return;
+  }
+  res.json(result);
+  console.log('hello')
+  });
+});
+
+/* GET later news api. */
+router.get('/later_news/:page', function(req, res, next) {
+  userGetSql = 'Select id,title,pubdate From `dede_archives` where channel=1 And arcrank = 0 order by id desc limit '+10*req.params.page+',10';
   connection.query(userGetSql,function (err, result) {
   if(err){
     console.log('[SELECT ERROR] - ',err.message);
